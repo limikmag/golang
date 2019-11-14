3e46d5fcaaa44b0b317176d6409524dc4f64e429
limikmag <limikmag@gmail.com>
2019-11-14 09:06:32 +0100

    Create singleton_thread_safe.go

---
+++ design_patterns/singleton/singleton_thread_safe.go
@@ -0,0 +1,20 @@
+package singleton
+/****************************************************************************************
+Singleton pattern represents a global state and most of the time reduces testability.   *
+*****************************************************************************************/
+
+type singleton map[string]string
+
+var (
+    once sync.Once
+    instance singleton
+)
+
+func New() singleton {
+	once.Do(func() {
+		instance = make(singleton)
+	})
+
+	return instance
+}
+
